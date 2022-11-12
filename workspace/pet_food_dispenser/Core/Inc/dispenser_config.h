9db4576ce0bd86fb975512ec9e7a0d93023ec120
Aaron Escoboza <EscobozaAaron@JohnDeere.com>
2022-11-12 15:46:58 -0600

    update hearbeat delay to 500 ms

--- workspace/pet_food_dispenser/Core/Inc/dispenser_config.h
+++ workspace/pet_food_dispenser/Core/Inc/dispenser_config.h
@@ -14,7 +14,7 @@
 #define HEART_BEAT_PRIORITY_TASK        1
 
 /* Delays*/
-#define DELAY_HEART_BEAT_TASK   1  /* In seconds */
+#define DELAY_HEART_BEAT_TASK   500 /* In ms */
 
 /* GPIO definitions */
 #define HEART_BEAT_LED          GPIOA, GPIO_PIN_5

48399f675bf2b8f05993cdc6807c20461f816b9d
Aaron Escoboza <EscobozaAaron@JohnDeere.com>
2022-11-17 00:29:25 -0600

    add tool for timers and do some clean up

--- workspace/pet_food_dispenser/thirdParty/buzzer/buzzer.c
+++ workspace/pet_food_dispenser/thirdParty/buzzer/buzzer.c
@@ -4,33 +4,30 @@ TIM_HandleTypeDef timHandler;
 
 HAL_StatusTypeDef buzzerInit(void)
 {
-
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     TIM_OC_InitTypeDef channelConfig;
 
     /* Clocks enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_TIM2_CLK_ENABLE();
-
-    /* Pin configuration */
+    /* Pin settings */
     GPIO_InitStruct.Pin = BUZZER_GPIO_PIN_NUM;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(BUZZER_GPIO_INSTANCE, &GPIO_InitStruct);
-
-    /* Timer configuration */
+    /* Timer settings */
     timHandler.Instance = BUZZER_TIMER_INSTANCE;
     timHandler.Channel = BUZZER_TIMER_CHANNEL;
     timHandler.Init.Prescaler = TIM_CLOCKDIVISION_DIV1;
-    timHandler.Init.Period = 0;
+    timHandler.Init.Period = 0xFFFFFFFF;
     timHandler.Init.CounterMode = TIM_COUNTERMODE_UP;
     if (HAL_TIM_OC_Init(&timHandler) != HAL_OK)
     {
         return HAL_ERROR;
     }
-
+    /* Channel settings */
     channelConfig.OCMode = TIM_OCMODE_TOGGLE;
     channelConfig.OCPolarity = TIM_OCNPOLARITY_HIGH;
     channelConfig.Pulse = 0;
@@ -38,7 +35,6 @@ HAL_StatusTypeDef buzzerInit(void)
     {
         return HAL_ERROR;
     }
-
     /* Interrupt settings */
     HAL_NVIC_SetPriority(TIM2_IRQn, 15, 0);
     HAL_NVIC_EnableIRQ(TIM2_IRQn);
--- workspace/pet_food_dispenser/thirdParty/buzzer/buzzer.h
+++ workspace/pet_food_dispenser/thirdParty/buzzer/buzzer.h
@@ -22,7 +22,6 @@
 #define BUZZER_GPIO_ALTERNATE         GPIO_AF1_LPTIM
 #define BUZZER_TIMER_INSTANCE         TIM2
 #define BUZZER_TIMER_CHANNEL          TIM_CHANNEL_1
-#define BUZZER_FREQUENCY              1000 /* Khz*/
 
 HAL_StatusTypeDef buzzerInit(void);
 HAL_StatusTypeDef buzzerEnable(void);

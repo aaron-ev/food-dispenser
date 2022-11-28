72d577b9783a61ff2074c48ae8617ce32bdc1341
Aaron Escoboza <>
2022-11-27 20:13:14 -0700

    update function signatures

--- workspace/pet_food_dispenser/App/Src/buzzer.c
+++ workspace/pet_food_dispenser/App/Src/buzzer.c
@@ -7,10 +7,8 @@
 #define MAX_PERIOD_COUNTER            250   /* MAX_PERIOD_COUNTER * buzzerTimPeriod = sound time */
 
 TimerHandle_t buzzerTimHandler;
-uint32_t buzzerTimPeriod = 1;        /* In ms, can't be grater less than 1ms */
-uint32_t callbackCounter = 0;
 
-HAL_StatusTypeDef buzzerPlay(void)
+HAL_StatusTypeDef buzzerStart(void)
 {
     BaseType_t retVal;
 
@@ -34,18 +32,18 @@ void buzzerStop(void)
     return HAL_OK;
 }
 
-HAL_StatusTypeDef buzzerInit(void)
+HAL_StatusTypeDef buzzerInit(uint32_t buzzerTimPeriod, uint32_t pin, GPIO_TypeDef *GPIOx)
 {
     GPIO_InitTypeDef gpioInit = {0};
 
     /* Clocks enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /* Pin settings */
-    gpioInit.Pin = BUZZER_GPIO_PIN_NUM;
+    gpioInit.Pin = pin;
     gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
     gpioInit.Pull = GPIO_PULLUP;
     gpioInit.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(BUZZER_GPIO_INSTANCE, &gpioInit);
+    HAL_GPIO_Init(GPIOx, &gpioInit);
 
     /* Create a one-shot timer */
     buzzerTimHandler = xTimerCreate("Buzzer-timer", pdMS_TO_TICKS(buzzerTimPeriod), pdFALSE, 0, buzzerTimCallback);

1c97b24792febef7393a3a8c88b7ecab250407a8
Aarón Escoboza <84993491+aaron-ev@users.noreply.github.com>
2022-11-28 19:10:34 -0600

    Feature/buzzer apis (#5)
    Buzzer refactoring

--- workspace/pet_food_dispenser/App/Inc/buzzer.h
+++ workspace/pet_food_dispenser/App/Inc/buzzer.h
@@ -12,8 +12,10 @@
 #ifndef __BUZZER__H
 #define __BUZZER_H
 #include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
 
-HAL_StatusTypeDef buzzerInit(void);
-HAL_StatusTypeDef buzzerPlay(void);
+HAL_StatusTypeDef buzzerInit(uint32_t buzzerTimPeriod, uint32_t pin, GPIO_TypeDef *GPIOx);
+HAL_StatusTypeDef buzzerStart(void);
+void buzzerStop(void);
 
 #endif
--- workspace/pet_food_dispenser/App/Src/buzzer.c
+++ workspace/pet_food_dispenser/App/Src/buzzer.c
@@ -1,58 +1,51 @@
-#include <buzzer.h>
+#include "buzzer.h"
 #include "FreeRTOS.h"
 #include "timers.h"
 
-TIM_HandleTypeDef timHandler;
-
-volatile uint32_t pulse_value = 16000; //to produce 100Khz
-volatile uint32_t ccr;
-
 #define BUZZER_GPIO_INSTANCE          GPIOA
 #define BUZZER_GPIO_PIN_NUM           GPIO_PIN_1
-#define MAX_PERIOD_COUNTER 250        /* MAX_PERIOD_COUNTER * buzzerTimPeriod = sound time */
+#define MAX_PERIOD_COUNTER            250   /* MAX_PERIOD_COUNTER * buzzerTimPeriod = sound time */
 
-TIM_HandleTypeDef timHandler;
 TimerHandle_t buzzerTimHandler;
-static uint32_t buzzerTimPeriod = 1;    /* ms */
-static uint32_t callbackCounter = 0;
 
-void buzzerTimCallback(TimerHandle_t xTimer)
+HAL_StatusTypeDef buzzerStart(void)
 {
-    if ( callbackCounter <= MAX_PERIOD_COUNTER )
-    {
-        HAL_GPIO_TogglePin(BUZZER_GPIO_INSTANCE, BUZZER_GPIO_PIN_NUM);
-        if (xTimerReset(buzzerTimHandler, 0) != pdPASS)
-        {
-            //todo: try, handle error.
-        }
-        callbackCounter++;
-    }
-}
+    BaseType_t retVal;
 
-HAL_StatusTypeDef buzzerPlay(void)
-{
-    callbackCounter = 0;
-    if (xTimerReset(buzzerTimHandler, 0) != pdPASS)
+    retVal = xTimerReset(buzzerTimHandler, 0);
+    if (retVal!= pdPASS)
     {
         return HAL_ERROR;
     }
     return HAL_OK;
 }
 
-HAL_StatusTypeDef buzzerInit(void)
+void buzzerStop(void)
+{
+    BaseType_t retVal;
+
+    retVal = xTimerStop(buzzerTimHandler, 0);
+    if (retVal!= pdPASS)
+    {
+        return HAL_ERROR;
+    }
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef buzzerInit(uint32_t buzzerTimPeriod, uint32_t pin, GPIO_TypeDef *GPIOx)
 {
     GPIO_InitTypeDef gpioInit = {0};
 
     /* Clocks enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /* Pin settings */
-    gpioInit.Pin = BUZZER_GPIO_PIN_NUM;
+    gpioInit.Pin = pin;
     gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
     gpioInit.Pull = GPIO_PULLUP;
     gpioInit.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(BUZZER_GPIO_INSTANCE, &gpioInit);
+    HAL_GPIO_Init(GPIOx, &gpioInit);
 
-    /* Create a one-shot timer to produce a square wave for buzzer sound */
+    /* Create a one-shot timer */
     buzzerTimHandler = xTimerCreate("Buzzer-timer", pdMS_TO_TICKS(buzzerTimPeriod), pdFALSE, 0, buzzerTimCallback);
     if ( buzzerTimHandler == NULL )
     {
@@ -60,4 +53,9 @@ HAL_StatusTypeDef buzzerInit(void)
     }
 
     return HAL_OK;
+}
+
+void buzzerTimCallback(TimerHandle_t xTimer)
+{
+    HAL_GPIO_TogglePin(BUZZER_GPIO_INSTANCE, BUZZER_GPIO_PIN_NUM);
 }

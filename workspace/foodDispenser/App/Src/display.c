bc65b2d12c323706d9bba2e87bef1c1fd7417893
Aaron Escoboza <>
2022-12-19 18:08:46 -0700

    add array of structures to handle better the indicator

--- workspace/foodDispenser/App/Src/display.c
+++ workspace/foodDispenser/App/Src/display.c
@@ -39,6 +39,9 @@ SPI_HandleTypeDef hspi1;
 #define DISPLAY_INDICATOR_BACK_X (153 - (DISPLAY_INDICATOR_H / 2))
 #define DISPLAY_INDICATOR_BACK_Y (189 - (DISPLAY_INDICATOR_W / 2))
 
+#define BEEP_DEFAULT_TON            100
+#define BEEP_DEFAULT_TOFF           BEEP_DEFAULT_TON
+
 /*
  * Available options for the first screen.
  */
@@ -50,6 +53,12 @@ typedef enum
     OPTION_FEED,
     OPTION_SETTINGS,
 } Options;
+
+typedef struct
+{
+    uint16_t x;
+    uint16_t y;
+} Coordinates;
 
 typedef struct
 {
@@ -102,67 +111,32 @@ BaseType_t displayRestartBacklightTimer(void)
     return status;
 }
 
-void displaySetIndicator(uint8_t option)
+#define MAX_OPTIONS                     5
+Coordinates indicatorPosition[MAX_OPTIONS];
+
+void initIndicatorPos(void)
 {
-    uint16_t xToSet;
-    uint16_t yToSet;
-    uint16_t optionClearX[3];
-    uint16_t optionClearY[3];
-    int i = 0;
+    // indicatorPosition[OPTION_PORTIONS].x = ;
+    // indicatorPosition[OPTION_PORTIONS].y = ;
+    // indicatorPosition[OPTION_SOUND].x = ;
+    // indicatorPosition[OPTION_SOUND].y = ;
+    // indicatorPosition[OPTION_BACK].x = ;
+    // indicatorPosition[OPTION_BACK].y = ;
+    indicatorPosition[OPTION_FEED].x = 100;
+    indicatorPosition[OPTION_FEED].y = 100;
+    indicatorPosition[OPTION_SETTINGS].x = 100;
+    indicatorPosition[OPTION_SETTINGS].y = 100;
+}
 
-    if (option == OPTION_FEED)
-    {
-        xToSet = DISPLAY_INDICATOR_FEED_X;
-        yToSet = DISPLAY_INDICATOR_FEED_Y;
-        optionClearX[0] = DISPLAY_INDICATOR_SETTINGS_X;
-        optionClearY[0] = DISPLAY_INDICATOR_SETTINGS_Y;
-        optionClearX[1] = '\0';
-    }
-//    if (option == OPTION_SETTINGS)
-//    {
-//        xToSet = DISPLAY_INDICATOR_SETTINGS_X;
-//        yToSet = DISPLAY_INDICATOR_SETTINGS_Y;
-//        optionClearX[0] = DISPLAY_INDICATOR_FEED_X;
-//        optionClearY[0] = DISPLAY_INDICATOR_FEED_Y;
-//        optionClearX[1] = '\0';
-//    }
-//    if (option == OPTION_PORTIONS)
-//    {
-//        xToSet = DISPLAY_INDICATOR_PORTIONS_X;
-//        yToSet = DISPLAY_INDICATOR_PORTIONS_Y;
-//        optionClearX[0] = DISPLAY_INDICATOR_SOUND_X;
-//        optionClearY[0] = DISPLAY_INDICATOR_SOUND_Y;
-//        optionClearX[1] = DISPLAY_INDICATOR_BACK_X;
-//        optionClearY[1] = DISPLAY_INDICATOR_BACK_Y;
-//        optionClearX[2] = '\0';
-//    }
-//    if (option == OPTION_SOUND)
-//    {
-//        xToSet = DISPLAY_INDICATOR_SOUND_X;
-//        yToSet = DISPLAY_INDICATOR_SOUND_Y;
-//        optionClearX[0] = DISPLAY_INDICATOR_PORTIONS_X;
-//        optionClearY[0] = DISPLAY_INDICATOR_PORTIONS_Y;
-//        optionClearX[1] = DISPLAY_INDICATOR_BACK_X;
-//        optionClearY[1] = DISPLAY_INDICATOR_BACK_Y;
-//        optionClearX[2] = '\0';
-//    }
-//    if (option == OPTION_BACK)
-//    {
-//        xToSet = DISPLAY_INDICATOR_BACK_X;
-//        yToSet = DISPLAY_INDICATOR_BACK_Y;
-//        optionClearX[0] = DISPLAY_INDICATOR_PORTIONS_X;
-//        optionClearY[0] = DISPLAY_INDICATOR_PORTIONS_Y;
-//        optionClearX[1] = DISPLAY_INDICATOR_SOUND_X;
-//        optionClearY[1] = DISPLAY_INDICATOR_SOUND_Y;
-//        optionClearX[2] = '\0';
-//    }
 
-    while (optionClearX != '\0')
-    {
-        tft_ili9341_fill_rectangle(optionClearX[i], optionClearY[i], DISPLAY_INDICATOR_W - 1, DISPLAY_INDICATOR_H - 1, WHITE);
-        i++;
-    }
-    tft_ili9341_fill_rectangle(xToSet, yToSet, DISPLAY_INDICATOR_W - 1, DISPLAY_INDICATOR_H - 1, GREEN);
+void displaySetIndicator(Options newPos, Options oldPos)
+{
+    /* Clear old position */
+    tft_ili9341_fill_rectangle(indicatorPosition[oldPos].x, indicatorPosition[oldPos].y,
+                               DISPLAY_INDICATOR_W, DISPLAY_INDICATOR_H, WHITE);
+    /* Set the new cursor */
+    tft_ili9341_fill_rectangle(indicatorPosition[newPos].x, indicatorPosition[newPos].y,
+                               DISPLAY_INDICATOR_W, DISPLAY_INDICATOR_H, GREEN);
 }
 
 void backLightCallback(TimerHandle_t xTimer)
@@ -207,7 +181,7 @@ void displayShowInitScreen(void)
                      BACKGROUND_RIGHT_W, BACKGROUND_RIGHT_H, (const uint16_t *)background_right);
     displayShowImage(42, 50, BUTTON_FEED_W, BUTTON_FEED_H, (const uint16_t *)button_feed);
     displayShowImage(42, 160, BUTTON_SETTINGS_W, BUTTON_SETTINGS_H, (const uint16_t *)button_settings);
-//    displaySetIndicator(OPTION_FEED);
+    displaySetIndicator(OPTION_FEED, OPTION_SETTINGS);
 }
 
 void displayInit(void)
@@ -223,7 +197,7 @@ void feed(uint8_t portions)
     mspDisableButtonInterrupts();
     if ((portions == 0) || (portions > DISPENSER_MAX_PORTIONS))
     {
-        dispenserBeep(100, 100, 3);
+        dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 3);
         return;
     }
 
@@ -243,83 +217,85 @@ void feed(uint8_t portions)
 
 void screenSettings(void)
 {
-    char buff[4];
-    uint32_t buttonEvent;
-    uint8_t options[] = {OPTION_BACK, OPTION_SOUND, OPTION_PORTIONS};
-    uint8_t minOption = options[0];
-    uint8_t maxOption = options[sizeof(options) - 1];
-    uint8_t index = sizeof(options) - 1; /* Max index by default */
-    uint8_t optionSelected = options[index];
+//     char buff[4];
+//     uint32_t buttonEvent;
+//     uint8_t options[] = {OPTION_BACK, OPTION_SOUND, OPTION_PORTIONS};
+//     uint8_t minOption = options[0];
+//     uint8_t maxOption = options[sizeof(options) - 1];
+//     uint8_t index = sizeof(options) - 1; /* Max index by default */
+//     uint8_t optionSelected = options[index];
 
-    // displayShowImage((const uint16_t *)settingsScreen);
-    while (1)
-    {
-        /* Wait until a push button is pressed.
-         * Notification settings: Index > 0, no clear on entry,
-         *                       Clear all events on exit,
-         *                       Block until there is an event.
-         */
-        xTaskNotifyWaitIndexed(BUTTON_INDEX_NOTIFICATION, NO_CLEAR_ON_ENTRY, CLEAR_ALL_ON_EXIT, &buttonEvent, portMAX_DELAY);
-        /* Handle button ENTER event */
-        if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_PORTIONS))
-        {
-            dispenserSettings.portions++;
-            if (dispenserSettings.portions > DISPENSER_MAX_PORTIONS)
-            {
-                dispenserSettings.portions = 1;
-            }
-            sprintf(buff, "%d", dispenserSettings.portions);
-            /* Portion option */
-            tft_ili9341_send_str(100, 200, buff, Font_16x26, BLACK, WHITE);
-        }
-        /* Option sound is selected */
-        if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_SOUND))
-        {
-            /* Toggle the sound state*/
-            dispenserSettings.sound ^= 1;
-            (dispenserSettings.sound == DISPENSER_SOUND_ON) ? sprintf(buff, "ON") : sprintf(buff, "OFF");
-            tft_ili9341_send_str(100, 200, buff, Font_16x26, BLACK, WHITE);
-        }
-        /* Option back is selected */
-        if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_BACK))
-        {
-            dispenserBeep(100, 100, 1);
-            displayShowInitScreen();
-            break;
-        }
+//     // displayShowImage((const uint16_t *)settingsScreen);
+//     while (1)
+//     {
+//         /* Wait until a push button is pressed.
+//          * Notification settings: Index > 0, no clear on entry,
+//          *                       Clear all events on exit,
+//          *                       Block until there is an event.
+//          */
+//         xTaskNotifyWaitIndexed(BUTTON_INDEX_NOTIFICATION, NO_CLEAR_ON_ENTRY, CLEAR_ALL_ON_EXIT, &buttonEvent, portMAX_DELAY);
+//         /* Handle button ENTER event */
+//         if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_PORTIONS))
+//         {
+//             dispenserSettings.portions++;
+//             if (dispenserSettings.portions > DISPENSER_MAX_PORTIONS)
+//             {
+//                 dispenserSettings.portions = 1;
+//             }
+//             sprintf(buff, "%d", dispenserSettings.portions);
+//             /* Portion option */
+//             tft_ili9341_send_str(100, 200, buff, Font_16x26, BLACK, WHITE);
+//         }
+//         /* Option sound is selected */
+//         if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_SOUND))
+//         {
+//             /* Toggle the sound state*/
+//             dispenserSettings.sound ^= 1;
+//             (dispenserSettings.sound == DISPENSER_SOUND_ON) ? sprintf(buff, "ON") : sprintf(buff, "OFF");
+//             tft_ili9341_send_str(100, 200, buff, Font_16x26, BLACK, WHITE);
+//         }
+//         /* Option back is selected */
+//         if ((buttonEvent & BUTTON_EVENT_ENTER) && (optionSelected == OPTION_BACK))
+//         {
+//             dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 1);
+//             displayShowInitScreen();
+//             break;
+//         }
 
-        /* Handle button UP event */
-        if (buttonEvent & BUTTON_EVENT_UP && (optionSelected != maxOption))
-        {
-            optionSelected = options[++index];
-            displaySetIndicator(optionSelected);
-        }
-        if (buttonEvent & BUTTON_EVENT_UP && (optionSelected == maxOption))
-        {
-            dispenserBeep(50, 50, 2);
-        }
+//         /* Handle button UP event */
+//         if (buttonEvent & BUTTON_EVENT_UP && (optionSelected != maxOption))
+//         {
+//             optionSelected = options[++index];
+//             displaySetIndicator(options[++index], options[index]);
+//         }
+//         if (buttonEvent & BUTTON_EVENT_UP && (optionSelected == maxOption))
+//         {
+//             dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 2);
+//         }
 
-        /* Handle button DOWN event */
-        if ((buttonEvent & BUTTON_EVENT_DOWN) && (optionSelected != minOption))
-        {
-            optionSelected = options[--index];
-            displaySetIndicator(optionSelected);
-        }
-        if (buttonEvent & BUTTON_EVENT_DOWN && (optionSelected == minOption))
-        {
-            dispenserBeep(50, 50, 2);
-        }
+//         /* Handle button DOWN event */
+//         if ((buttonEvent & BUTTON_EVENT_DOWN) && (optionSelected != minOption))
+//         {
+//             optionSelected = options[--index];
+//             displaySetIndicator(options[--index], options[index]);
+//         }
+//         if (buttonEvent & BUTTON_EVENT_DOWN && (optionSelected == minOption))
+//         {
+//             dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 2);
+//         }
 
-        /* Update cursor */
-        displaySetIndicator(optionSelected);
-    }
+//         /* Update cursor */
+//         displaySetIndicator(optionSelected);
+//     }
+// }
 }
+
 
 void vTaskDisplay(void *params)
 {
     BaseType_t status;
     uint32_t buttonEvent;
-    uint8_t cursorPosition = OPTION_FEED;
+    Options cursorIndicator = OPTION_FEED;
 
     /* Show init screen */
     displayShowInitScreen();
@@ -352,40 +328,36 @@ void vTaskDisplay(void *params)
             continue;
         }
 
-        if ((buttonEvent & BUTTON_EVENT_ENTER) && (cursorPosition == OPTION_FEED))
+        /* Handle button events: ENTER, UP and DOWN */
+        if ((buttonEvent & BUTTON_EVENT_ENTER) && (cursorIndicator == OPTION_FEED))
         {
-            dispenserBeep(100, 100, 1);
-            /* Update screen with a feed message */
-            tft_ili9341_fill_screen(BLACK);
-            tft_ili9341_send_str(TFT_ILI9341_WIDTH / 4 - 20, TFT_ILI9341_HEIGHT / 2, "Feeding...", Font_16x26, WHITE, BLACK);
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 1);
             feed(dispenserSettings.portions);
-            displayShowInitScreen();
-            cursorPosition = OPTION_FEED;
         }
-        if ((buttonEvent & BUTTON_EVENT_ENTER) && (cursorPosition == OPTION_SETTINGS))
+        if ((buttonEvent & BUTTON_EVENT_ENTER) && (cursorIndicator == OPTION_SETTINGS))
         {
-            dispenserBeep(100, 100, 3);
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 3);
             screenSettings();
         }
-        if ((buttonEvent & BUTTON_EVENT_UP) && (cursorPosition == OPTION_FEED))
+        if ((buttonEvent & BUTTON_EVENT_UP) && (cursorIndicator == OPTION_FEED))
         {
-            dispenserBeep(50, 50, 2);
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 2);
         }
-        if ((buttonEvent & BUTTON_EVENT_UP) && (cursorPosition == OPTION_SETTINGS))
+        if ((buttonEvent & BUTTON_EVENT_UP) && (cursorIndicator == OPTION_SETTINGS))
         {
-            dispenserBeep(100, 100, 1);
-            displaySetIndicator(OPTION_FEED);
-            cursorPosition = OPTION_FEED;
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 1);
+            displaySetIndicator(OPTION_FEED, OPTION_SETTINGS);
+            cursorIndicator = OPTION_FEED;
         }
-        if ((buttonEvent & BUTTON_EVENT_DOWN) && (cursorPosition == OPTION_FEED))
+        if ((buttonEvent & BUTTON_EVENT_DOWN) && (cursorIndicator == OPTION_FEED))
         {
-            displaySetIndicator(OPTION_SETTINGS);
-            dispenserBeep(100, 100, 1);
-            cursorPosition = OPTION_SETTINGS;
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 1);
+            displaySetIndicator(OPTION_SETTINGS, OPTION_FEED);
+            cursorIndicator = OPTION_SETTINGS;
         }
-        if ((buttonEvent & BUTTON_EVENT_DOWN) && (cursorPosition == OPTION_SETTINGS))
+        if ((buttonEvent & BUTTON_EVENT_DOWN) && (cursorIndicator == OPTION_SETTINGS))
         {
-            dispenserBeep(50, 50, 2);
+            dispenserBeep(BEEP_DEFAULT_TON, BEEP_DEFAULT_TOFF, 2);
         }
     }
 }
